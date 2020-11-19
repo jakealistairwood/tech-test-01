4f561f3db6b9c239cb1452778f66a34daca60327
Jake Wood <jakealistairwood@gmail.com>
2020-11-19 11:36:54 +0000

    adding formula to convert kelvin to celsius

--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -5,19 +5,36 @@ const CityCard = (props) => {
 
     const { bristol, rome, london } = props;
 
-    // To convert Kelvin to Celsius the formula is:
-    // K - 273.15 = _'C
-    const kelvinToCelsius = () => {
+    console.log(bristol.main.temp);
 
-    }
+    // Kelvin to Celsius formula is:
+    // K - 273.15 = _'C
+
+    const tempInBristol = Math.round(bristol.main.temp - 273.15);
+    const tempInLondon = Math.round(london.main.temp - 273.15);
+    const tempInRome = Math.round(rome.main.temp - 273.15);
 
     return (
+        <>
         <div className={styles.londonCard}>
             <header className={styles.cityHeader}>
                 <h3>London</h3>
-                <p></p>
+                <p>{tempInLondon}&deg;C</p>
             </header>    
         </div>
+        <div className={styles.bristolCard}>
+            <header className={styles.cityHeader}>
+                <h3>Bristol</h3>
+                <p>{tempInBristol}&deg;C</p>
+            </header>    
+        </div>
+        <div className={styles.romeCard}>
+            <header className={styles.cityHeader}>
+                <h3>Rome</h3>
+                <p>{tempInRome}&deg;C</p>
+            </header>    
+        </div>
+        </>
     )
 }
 
